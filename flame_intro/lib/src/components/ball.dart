80b488a1c5fb3cd999c29f817d5205b7023a8ed5
Efe Yenice <efeyenice004@gmail.com>
2025-07-11 16:16:24 +0300

    create bat and effects

--- flame_intro/lib/src/components/ball.dart
+++ flame_intro/lib/src/components/ball.dart
@@ -1,7 +1,9 @@
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/collisions.dart';    
+import 'package:flame/effects.dart';  
 
+import 'bat.dart';  
 import '../brick_breaker.dart';                                 // And this import
 import 'play_area.dart';     
 
@@ -41,9 +43,15 @@ class Ball extends CircleComponent with CollisionCallbacks, HasGameReference<Bri
       } else if (intersectionPoints.first.x >= game.width) {
         velocity.x = -velocity.x;
       } else if (intersectionPoints.first.y >= game.height) {
-        removeFromParent();
+        add(RemoveEffect(delay: 0.35));
       }
-    } else {
+    } else if (other is Bat) {
+      velocity.y = -velocity.y;
+      velocity.x =
+          velocity.x +
+          (position.x - other.position.x) / other.size.x * game.width * 0.3;
+    }
+    else {
       debugPrint('collision with $other');
     }
   }
