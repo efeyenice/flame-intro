20c29a7a8e91b84e96cc7e67d62a4cc21575643a
Efe Yenice <efeyenice004@gmail.com>
2025-07-11 15:20:15 +0300

    add ball bounce on collision with play_area

--- flame_intro/lib/src/components/play_area.dart
+++ flame_intro/lib/src/components/play_area.dart
@@ -3,10 +3,14 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flame/collisions.dart';
 import '../brick_breaker.dart';
 
 class PlayArea extends RectangleComponent with HasGameReference<BrickBreaker> {
-  PlayArea() : super(paint: Paint()..color = const Color(0xfff2e8cf));
+  PlayArea() : super(
+    paint: Paint()..color = const Color(0xfff2e8cf),
+    children: [RectangleHitbox()],
+  );
 
   @override
   FutureOr<void> onLoad() async {
